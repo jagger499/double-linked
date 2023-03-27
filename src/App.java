94bd75cd61e5bf80d4e69101244046bdfb1ebbe8
jagg499 <jagg499@gmail.com>
2023-03-27 12:41:05 -0500

    delete comments in main

--- src/App.java
+++ src/App.java
@@ -2,9 +2,5 @@ public class App {
     public static void main(String[] args) throws Exception {
         MenuDoubleLinkedList MenuLinked = new MenuDoubleLinkedList();
         MenuLinked.exectMenu();
-        //  DoubleLinkedList Lista = new DoubleLinkedList();
-        // Lista.pushFirst("jhon");
-        // Lista.deleteElementByValue("jhon");
-        // Lista.displayList();
     }
 }
